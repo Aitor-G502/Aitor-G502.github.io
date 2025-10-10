5d0fc99294b4733852a0c1bef77469eecc95e452
Aitor-G502 <sm24.agarfias@iessantamariadeivissa.com>
2025-10-10 17:03:38 +0200

    v2.0

---
+++ Programacion/Ejercicio1.py
@@ -0,0 +1,8 @@
+# Leer un número por pantalla
+numero = int(input("Introduce un número: "))
+
+# Multiplicarlo por dos usando una variable
+resultado = numero * 2
+
+# Imprimir el resultado
+print("El resultado es:", resultado)
