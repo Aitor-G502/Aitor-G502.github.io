a1c54c11d2736f9ead965fe6497889c930293b3a
Aitor-G502 <sm24.agarfias@iessantamariadeivissa.com>
2026-01-14 20:06:09 +0100

    2.17

--- Programacion/Deberesbucles.py
+++ Programacion/Deberesbucles.py
@@ -40,7 +40,7 @@ numLin = 1
 
 while numLin <= n:
     print(f'{numLin},')
-    numLin = numLin + 1
+    numLin = numLin + 2
 
 # 3 Escriure un algorisme en Python que imprimeixi els 10 primers números parells començant a 2 i imprimiu també els seus respectius cubs. Per exemple: 2 – 8; 4 – 64; 6 – 216 …
 #Modifica aquest programa perquè en comptes d’imprimir els 10 primers nombres parells, imprimeixi els primers n nombres parells on n es un enter positiu introduït per teclat.
@@ -57,7 +57,26 @@ while contador <= numpar:
     num = num + 2
     contador = contador + 1
 
-# 5 Escriure un programa que demani a l'usuari un número sencer i mostri per pantalla un triangle rectangle com el de més avall, d'alçada el número introduït. La imatge següent mostra l’exemple de la sortida del programa si introduïm el número 5.
+# 4 Realitza un programa que primer, llegeixi una frase per teclat i segón, que compti el nombre de paraules que té aquesta frase. 
+# Recorda que hauràs d’utilitzar mètodes built-in de la classe String i de la clase List a més d’utilitzar el bucle for.
+
+num4 = input("Escribe tu frase aqui: ")
+
+listanum4 = list(num4)
+
+palabranum4 = 1
+
+i = 1
+
+for element in listanum4:
+    if element == " ":
+        palabranum4 = palabranum4 + 1
+    i = i +1
+print(f'Tu frase tiene {palabranum4} palabras')
+
+
+# 5 Escriure un programa que demani a l'usuari un número sencer i mostri per pantalla un triangle rectangle com el de més avall, d'alçada el número introduït. 
+# La imatge següent mostra l’exemple de la sortida del programa si introduïm el número 5.
 #* 
 #** 
 #*** 
@@ -84,22 +103,61 @@ while mult <= veces:
     print(f'{palabra}')
     mult = mult + 1
 
+# Es demanar a l'usuari que introdueixi una cadena de caràcters per teclat y el bucle for haurà de processar aquesta cadena
+# i crear-ne una de nova amb les lletres minúscules y majúscules alternades.
+deberes = input("Escribe tu cadena de caracteres aqui: ")
 
+listadeberes = list(deberes)
 
-# 11 Imprimeix el següent patró utilitzant el bucle for, les dimensions del patró dependran d’un nombre introduït per teclat
+nuevadeberes = []
 
-numero = int(input("Introduce un numero: "))
+palabradeberes = ""
 
-cantidad = 1
-patron = "*"
+ñ = 1
 
-while cantidad <= 2*numero - 1:
-    if cantidad <= numero:
-        print ((patron + "")*cantidad)
+for letradeberes in listadeberes:
+    if ñ %2 == 0:
+        nuevadeberes.append(letradeberes.lower())
     else:
-        print((patron + "") * (2*numero - cantidad))
-    cantidad = cantidad + 1
+        nuevadeberes.append(letradeberes.upper())
+    ñ = ñ+1
+for letradeberes2 in nuevadeberes:
+    palabradeberes = palabradeberes + letradeberes2
+print(f'{palabradeberes}')
 
 
 
+cadena = input("Escribe tu cadena de caracteres: ")
 
+cadenaalt = ""
+
+ismayus = True
+
+for char in cadena:
+    if ismayus:
+        cadenaalt += char.upper()
+    else:
+        cadenaalt += char.lower()
+    ismayus = not ismayus
+
+print(f'La cadena resultante de {cadena} es:{cadenaalt}')
+
+print()
+
+cadena2 = input("Escripe tu cadena de caracteres: ")
+
+cadenaalt2 = ""
+
+ismayus2 = True
+
+indice = 0
+
+while indice <= len(cadena2) -1:
+    if ismayus2:
+        cadenaalt2 += cadena[indice].upper()
+    else:
+        cadenaalt2 += cadena[indice].lower()
+    ismayus2 = not ismayus2
+    indice += 1
+
+    print(f'La cadena resultante de {cadena2} es:{cadenaalt2}.')
