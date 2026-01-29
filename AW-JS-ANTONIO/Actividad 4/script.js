0c839a375b357cda0b2b29058679cca53cf7f715
Aitor-G502 <sm24.agarfias@iessantamariadeivissa.com>
2026-01-29 14:23:32 +0100

    2.23

--- AW-JS-ANTONIO/Actividad 4/script.js
+++ AW-JS-ANTONIO/Actividad 4/script.js
@@ -1,22 +1,22 @@
 //SECTOR 1 //
 
 
-function cambiarTexto() {
-   const textoEjemplo = document.getElementById("textoEjemplo");
-textoEjemplo.style.backgroundColor = "beige";
+function cambiarTexto() {                                          // crea una funcion llamada cambiartexto
+   const textoEjemplo = document.getElementById("textoEjemplo"); //crea una variable y busca los elementos con la id textoEjemplo
+textoEjemplo.style.backgroundColor = "beige";                   // cambia el color de texto ejemplo
 
-textoEjemplo.textContent = "Hola, se ha modificado este texto";
+textoEjemplo.textContent = "Hola, se ha modificado este texto"; // modifica el texto de texto ejemp
 }
 function restaurarTexto() {
-   const textoEjemplo = document.getElementById("textoEjemplo");
+   const textoEjemplo = document.getElementById("textoEjemplo");   //crea una funcion que crea una variable que busca elementos por id que cambia su fondo a blanco
 textoEjemplo.style.backgroundColor = "white";
 
-textoEjemplo.textContent = "Este texto será modificado cuando hagas clic.";
+textoEjemplo.textContent = "Este texto será modificado cuando hagas clic.";  // 
 }
 
 // SECTOR 4 //
 
-const nuevoItem = document.getElementById("nuevoItem")
+const nuevoItem = document.getElementById("nuevoItem") // crea una nueva variable llamada nuevoItem y busca los elementos que tengan la ID nuevoItem
 
 // listaElementos.removeChild(li[100000])
 
@@ -24,14 +24,14 @@ const nuevoItem = document.getElementById("nuevoItem")
 // SECTOR 5 //
 
 function cambiarCajas(){
-    const cajas = document.querySelectorAll(".caja");
+    const cajas = document.querySelectorAll(".caja"); // Crea la variable cajas y busca todos los elementos que tengan la clase caja
     for (let i = 0; i < cajas.length; i++) {
-        cajas[i].style.backgroundColor = generarColorAleatorio();
+        cajas[i].style.backgroundColor = generarColorAleatorio();  //Bucle para que todas las clases cajas que no superen un limite tengan el color aleatorio
     }
 }
 
-document.querySelectorAll('.caja').forEach(caja => {
-    caja.addEventListener('mouseenter', () => caja.style.opacity ='0');
+document.querySelectorAll('.caja').forEach(caja => {       // Busca todos los elementos con la clase caja y por cada caja  crea eventos para que se vean o se vuelva invisible una caja
+    caja.addEventListener('mouseenter', () => caja.style.opacity ='0');  
     caja.addEventListener('mouseleave', () => caja.style.opacity ='1');
 });
 
