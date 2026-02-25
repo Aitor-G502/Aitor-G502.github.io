d04d70876a543185a50f3f10312637f46b66dfb3
Aitor-G502 <sm24.agarfias@iessantamariadeivissa.com>
2026-02-25 13:21:42 +0100

    2.29

--- Programacion/Deberes-18-02.py
+++ Programacion/Deberes-18-02.py
@@ -1,8 +1,16 @@
 # 2 Imagina que te han dado la frase = 'Hola como vas amigo mío.'
 
-frase = "Hola como vas amigo mío"
+frase = input("Introduce una frase: ").split()
 
-contador = 1
 
-while contador <= frase:
-    
+contador = 0
+contador2 = 0
+fraseinv = frase[::-1]
+
+while contador < len(frase):
+    print(" ".join(frase[:contador+1]))
+    contador += 1
+
+while contador2 < len(fraseinv):
+    print(" ".join(frase[contador2:]))
+    contador2 += 1
